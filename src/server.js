4c803efc93580a460acb615330f086c2ef94413c
amrinaas <amrinaas96@gmail.com>
2024-07-18 14:34:42 +0700

    Adding server.js

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import 'dotenv/config';
+
+import userRoutes from './routes/userRoutes.js'; // Ensure the path is correct
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/user', userRoutes);
+
+app.get('/', (req, res) =>
+  res.send({
+    message: 'App is running',
+  })
+);
+
+// START SERVER
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
