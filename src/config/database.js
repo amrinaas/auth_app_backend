ca1484f6ad0415c1c3e57483fa26d092c7c8e58b
amrinaas <amrinaas96@gmail.com>
2024-07-18 14:34:20 +0700

    Adding database

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+import sql from 'mysql2';
+
+const dbPool = sql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+export default dbPool.promise();
