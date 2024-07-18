1c575bd4ca57cc85315044269318e107b5a33e01
amrinaas <amrinaas96@gmail.com>
2024-07-18 22:38:05 +0700

    Add new user model function to get token and update

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,15 +1,21 @@
 import dbPool from '../config/database.js';
 
+const getConnection = async () => {
+  return await dbPool.getConnection();
+};
+
 const createUser = async (
+  connection,
   username,
   email,
   password,
   is_email_verified,
   sign_up_source,
-  sign_up_timestamps
+  sign_up_timestamps,
+  token
 ) => {
-  await dbPool.query(
-    'INSERT INTO users (username, email, password, is_email_verified, sign_up_source, sign_up_timestamps) VALUES (?, ?, ?, ?, ?, ?)',
+  await connection.query(
+    'INSERT INTO users (username, email, password, is_email_verified, sign_up_source, sign_up_timestamps, verification_token) VALUES (?, ?, ?, ?, ?, ?, ?)',
     [
       username,
       email,
@@ -17,6 +23,7 @@ const createUser = async (
       is_email_verified,
       sign_up_source,
       sign_up_timestamps,
+      token,
     ]
   );
 };
@@ -29,4 +36,35 @@ const checkEmailExist = async (email) => {
   return rows.length > 0;
 };
 
-export default { createUser, checkEmailExist };
+const findUserByToken = async (token) => {
+  const connection = await dbPool.getConnection();
+  try {
+    const [rows] = await connection.query(
+      'SELECT * FROM users WHERE verification_token = ?',
+      [token]
+    );
+
+    return rows[0];
+  } catch (error) {
+    throw error;
+  } finally {
+    connection.release();
+  }
+};
+
+const updateUserByToken = async (token, updates) => {
+  const { is_email_verified, verification_token } = updates;
+
+  await dbPool.query(
+    'UPDATE users SET is_email_verified = ?, verification_token =? WHERE verification_token = ?',
+    [is_email_verified, verification_token, token]
+  );
+};
+
+export default {
+  getConnection,
+  createUser,
+  checkEmailExist,
+  findUserByToken,
+  updateUserByToken,
+};
