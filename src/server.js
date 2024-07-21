8f560fd0fe53ae89f474a3f7ed16c4fdf9c9878f
amrinaas <amrinaas96@gmail.com>
2024-07-21 12:00:22 +0700

    Install new package

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-
+import passport from './config/passport.js';
+import session from 'express-session';
 import userRoutes from './routes/userRoutes.js'; // Ensure the path is correct
 
 const app = express();
@@ -13,6 +14,15 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: process.env.WEBSITE }));
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/user', userRoutes);
 
